e29fbcc9dd2d213f4ca531497981e907a5ab563c
Ken <kokiebisu@icloud.com>
2024-07-02 07:54:56 -0600

    intial commit

---
+++ go.mod
@@ -0,0 +1,29 @@
+module github.com/kokiebisu/mycontent
+
+go 1.22.4
+
+require (
+	entgo.io/ent v0.13.1
+	github.com/99designs/gqlgen v0.17.49
+	github.com/lib/pq v1.10.9
+	github.com/vektah/gqlparser/v2 v2.5.16
+)
+
+require (
+	ariga.io/atlas v0.19.1-0.20240203083654-5948b60a8e43 // indirect
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/zclconf/go-cty v1.8.0 // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
---
+++ packages/contents/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/kokiebisu/mycontent/packages/contents/ent"
+	"github.com/kokiebisu/mycontent/packages/contents/graph"
+	_ "github.com/lib/pq"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	client, err := ent.Open("postgres", "host=localhost port=5433 user=postgres dbname=mydb sslmode=disable password=mypassword")
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		Client: client,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
