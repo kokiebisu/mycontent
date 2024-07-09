4f46e212c3be6556cc88eaa5d90aef2d93dbfcfb
Ken <kokiebisu@icloud.com>
2024-07-09 15:28:38 -0600

    set up GetByEmail public method for user service

--- packages/service-user/port/service.go
+++ packages/service-user/port/service.go
@@ -12,6 +12,7 @@ type UserService interface {
 	Create(ctx context.Context, firstName string, lastName string, email string, password string, interest user.Interest, yearsOfExperience int, username string, publishTime string) (*ent.User, error)
 	Get(ctx context.Context, id string) (*ent.User, error)
 	GetAll(ctx context.Context) ([]*ent.User, error)
+	GetByEmail(ctx context.Context, email string) (*ent.User, error)
 	Update(ctx context.Context, id string, firstName string, lastName string, email string, password string, interest user.Interest, yearsOfExperience int, username string) (*ent.User, error)
 	Delete(ctx context.Context, id string) (string, error)
 }
