ffbc5f7830835c28902d79cdcbe56107b5cee7dd
Ken <kokiebisu@icloud.com>
2024-07-12 16:13:05 -0600

    add mechanism to fallback for config load

--- packages/service-blog/config/config.go
+++ packages/service-blog/config/config.go
@@ -1,13 +1,30 @@
 package config
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	// TODO: Add error handling
-	godotenv.Load("./config/.env")
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file: %v", err)
-	// }
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Printf("Warning: Error getting current working directory: %v", err)
+		return
+	}
+
+	// Construct the path to the .env file
+	envPath := filepath.Join(cwd, "packages", "service-blog", "config", ".env")
+
+	// Attempt to load the .env file
+	err = godotenv.Load(envPath)
+	if err != nil {
+		log.Printf("Warning: Error loading .env file: %v", err)
+		log.Println("Continuing without .env file. Make sure environment variables are set correctly.")
+	} else {
+		log.Println("Successfully loaded .env file")
+	}
 }
