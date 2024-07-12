f9967475054e211dc7e592ed3c077437ec1e73c7
Ken <kokiebisu@icloud.com>
2024-07-12 16:08:05 -0600

    temporarily prevent config loading from failing

--- packages/service-blog/config/config.go
+++ packages/service-blog/config/config.go
@@ -1,14 +1,13 @@
 package config
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load("./config/.env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	// TODO: Add error handling
+	godotenv.Load("./config/.env")
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %v", err)
+	// }
 }
