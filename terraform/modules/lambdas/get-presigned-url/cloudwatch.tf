44d17213de6ae8e99804025690aafbef0fdcac54
Ken <kokiebisu@icloud.com>
2024-08-27 02:28:15 +0900

    fix log group naming

--- terraform/modules/lambdas/get-presigned-url/cloudwatch.tf
+++ terraform/modules/lambdas/get-presigned-url/cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "parse_conversations" {
-  name              = "/aws/lambda/${var.environment}/${aws_lambda_function.get_presigned_url.function_name}"
+resource "aws_cloudwatch_log_group" "get_presigned_url" {
+  name              = "/aws/lambda/${var.environment}/get-presigned-url"
   retention_in_days = 1
 }
--- terraform/modules/lambdas/parse_conversations/cloudwatch.tf
+++ terraform/modules/lambdas/parse_conversations/cloudwatch.tf
@@ -1,4 +1,3 @@
 resource "aws_cloudwatch_log_group" "parse_conversations" {
-  name              = "/aws/lambda/${var.environment}/saga-blog-processor/${aws_lambda_function.parse_conversations.function_name}"
-  retention_in_days = 1
+  name              = "/aws/lambda/${var.environment}/parse-conversations"
 }
